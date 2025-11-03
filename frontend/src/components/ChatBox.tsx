de30715b2da3f7909469028c248529bfebee66ec
auberginewly <3127221787@qq.com>
2025-11-03 22:51:22 +0800

    feat: 重构 ChatBox 组件，添加消息状态管理和提示页面，优化用户体验

--- frontend/src/components/ChatBox.tsx
+++ frontend/src/components/ChatBox.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
+import {useAppContext} from '../context/AppContext'
+
+import assets from '../assets/assets'
+import Message from './Message'
+
+
+interface Message {
+  id: string
+  role: 'user' | 'assistant'
+  content: string
+  timestamp: Date
+}
 
 const ChatBox = () => {
+  const {selectedChat, theme} = useAppContext()
+  const [messages, setMessages] = useState<Message[]>([])
+  const [loading, setLoading] = useState<boolean>(false)  
+
+
+
+  useEffect(() => {
+    if (selectedChat) {
+      setMessages(selectedChat.messages)
+    }
+  }, [selectedChat])
+
   return (
-    <div>
+    <div className='flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40'>
       
+      {/* 聊天消息 */}
+      <div className='flex-1 mb-5 overflow-y-scroll'>
+
+        {/* 提示页面 */}
+        {messages.length === 0 && (
+          <div className='h-full flex flex-col items-center justify-center gap-2 text-primary'>
+            <img src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark} />
+            <p className='mt-5 text-4xl sm:text-6xl text-center text-gray-400 dark:text-white'>
+              有什么想问的吗？
+            </p>
+          </div>
+        )}
+
+        {/* 消息列表 */}
+        {messages.map((msg,index) =><Message key={index} message={msg}/>) }
+      </div>
+
+      {/* prompt 输入框 */}
+      <form action="">
+
+      </form>
     </div>
   )
 }
